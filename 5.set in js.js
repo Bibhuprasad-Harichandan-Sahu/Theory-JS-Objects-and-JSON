f2c64dc0318d6b5f4b632d98a056adb3df7e9aab
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-15 17:15:34 +0530

    set method in js

---
+++ 5.set in js.js
@@ -0,0 +1,12 @@
+let arr = [1,2,2,3,3,4,5,5,6];
+console.log(arr);//1, 2, 2, 3, 3,4, 5, 5, 6
+//but in set it will not print duplicate items
+console.log(arr.length);//9
+
+let set = new Set([1,2,2,3,3,4,5,5,6]);
+console.log(set.size);//6
+console.log(set);//Set(6) { 1, 2, 3, 4, 5, 6 }
+
+
+
+
