7e5b41e1bd549ca7af0ed2827661a4e081c658e4
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-15 12:10:39 +0530

    1. Using dot Notation & Bracket notation

---
+++ 1.Accessing Object Properties.js
@@ -0,0 +1,16 @@
+const person = {
+    name: 'John',
+    age: 20,
+};
+
+// accessing property
+console.log(person.name); // John
+
+let Cars = {
+    Maruti :"Ciaz",
+    Hyundai :"Verna",
+    Honda :"City"
+};
+console.log(Cars);//{ Maruti: 'Ciaz', Hyundai: 'Verna', Honda: 'City' }
+console.log(Cars.Hyundai);//Verna
+console.log(Cars["Honda"]);//City
